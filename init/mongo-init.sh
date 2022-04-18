e858d898807f8bb48267b9dabea139387c00d1bb
techatdhq <31241135+techatdhq@users.noreply.github.com>
2022-04-18 15:15:11 +0530

    Build 1.0.0

---
+++ init/mongo-init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+mongo <<EOF
+
+use admin
+
+db.createUser({
+  user: '$MONGO_INITDB_USER',
+  pwd: '$MONGO_INITDB_PWD',
+  roles: [{
+    role: "userAdminAnyDatabase",
+    db: "admin" 
+  }, {
+    role: 'readWrite',
+    db: 'db5x_studio'
+  }]
+})
+
+EOF
+
+mongorestore /mongodump
