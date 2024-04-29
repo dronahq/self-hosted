db80d948a9312ec973bb976594dc1c6f36b46460
rahul-gharat <42729746+rahul-gharat@users.noreply.github.com>
2024-04-29 16:18:13 +0530

    file updates for dronahq 2.4.6

--- container-reset.sh
+++ container-reset.sh
@@ -1,4 +1,5 @@
-sudo docker-compose pull
-sudo docker-compose down
-sudo docker-compose up -d
+sudo docker-compose pull webapp
+sudo docker-compose stop webapp
+sudo docker-compose rm webapp
+sudo docker-compose up -d webapp
 sudo docker system prune
