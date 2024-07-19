9971571e306f52c96ba1c6d137f0221283736401
rahul-gharat <rahul@dronamobile.com>
2024-07-19 16:57:02 +0530

    example file added

--- get-docker-compose.sh
+++ get-docker-compose.sh
@@ -1,2 +1,3 @@
-sudo -E curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+# sudo -E curl -L https://github.com/docker/compose/releases/download/2.28.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.29.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
